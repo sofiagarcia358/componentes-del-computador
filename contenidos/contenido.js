955d775182995a723c8adaa962760abf8f103fe0
keneth <keneth.javier90@gmail.com>
2024-05-02 20:41:27 -0600

    keneth version 01

---
+++ contenidos/contenido.js
@@ -0,0 +1,20 @@
+var liElement = document.querySelector(".p1");
+liElement.innerHTML = 'el cpu es el cerebro de una computadora. Es un chip o conjunto de chips que ejecutan instrucciones y realiza cálculos para llevar a cabo todas las tareas que realizamos en una computadora, desde navegar por la web hasta editar documentos y jugar videojuegos. El CPU interpreta y ejecuta instrucciones de programas, realiza operaciones matemáticas y lógicas, y coordina el funcionamiento de otros componentes del sistema. En esencia, el CPU es el corazón de la computadora.';
+
+var liElement = document.querySelector(".car1");
+liElement.innerHTML = 'Velocidad de reloj'
+
+var liElement = document.querySelector(".car2");
+liElement.innerHTML = 'Nucelos'
+
+var liElement = document.querySelector(".car3");
+liElement.innerHTML = 'Caché'
+
+var liElement = document.querySelector(".car4");
+liElement.innerHTML = 'Tecnología de proceso'
+
+var liElement = document.querySelector(".car5");
+liElement.innerHTML = 'Arquitectura'
+
+var liElement = document.querySelector(".car6");
+liElement.innerHTML = 'Soporte de instrucciones'
---
+++ menu lateral/menu_lateral.js
@@ -0,0 +1,16 @@
+var liElement = document.querySelector(".a2");
+
+
+liElement.innerHTML = 'cpu';
+
+var liElement = document.querySelector(".a3");
+liElement.innerHTML = 'gpu'
+
+var liElement = document.querySelector(".a4");
+liElement.innerHTML = 'md'
+
+var liElement = document.querySelector(".a5");
+liElement.innerHTML = 'RAM'
+
+var liElement = document.querySelector(".a6");
+liElement.innerHTML = 'HDD'
