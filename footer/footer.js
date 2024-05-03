d10c281e345d3d06b132f1d6df4de8ed442d8554
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-02 19:54:08 -0600

    sofia1

---
+++ footer/footer.js
@@ -0,0 +1,7 @@
+let footer_sof = document.querySelector("section");
+
+let footer_sofi = document.querySelector(".frase");
+footer_sofi.innerHTML = 'Bill Gates Dijo una vez:"El ordenador nació para resolver problemas que antes no existían"';
+
+let footer_sofis = document.querySelector(".participantes");
+footer_sofis.innerHTML = 'Kennet Calderon y Sofía García, ambos de 4to computación';
---
+++ header/header.js
@@ -0,0 +1,7 @@
+let header_sof = document.querySelector("header");
+
+let header_sofi = document.querySelector(".inicio");
+header_sofi.innerHTML = '¡Bienvenidos!';
+
+let header_sofis = document.querySelector(".texto");
+header_sofis.innerHTML = 'componentes del computador: ';
