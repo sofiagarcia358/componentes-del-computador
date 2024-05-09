254f4105e73c8b8bdc9aa15e0ca687a9169f9895
keneth <keneth.javier90@gmail.com>
2024-05-09 10:15:12 -0600

    gpu version keneth

---
+++ contenido_gpu/gpu.js
@@ -0,0 +1,13 @@
+let section = document.querySelector(".section_gpu");
+section.innerHTML = `
+<h1 class="gpu1">GPU</h1>
+<p class="gpu2">Las unidades de procesamiento gráfico (GPU) son componentes esenciales en las computadoras modernas, especialmente en el ámbito de la visualización y el rendimiento de gráficos en 3D. Estas unidades están diseñadas específicamente para procesar rápidamente grandes cantidades de datos relacionados con gráficos y renderizado. Las GPU son fundamentales no solo en juegos y aplicaciones multimedia, sino también en campos como la inteligencia artificial, el aprendizaje automático y la ciencia de datos, donde su capacidad para realizar cálculos en paralelo proporciona una ventaja significativa en términos de velocidad y eficiencia. Con avances continuos en tecnología y arquitectura, las GPU continúan desempeñando un papel crucial en la informática contemporánea, impulsando la innovación y el rendimiento en una variedad de aplicaciones y disciplinas.</p>
+<div class="img1"><img src="https://i.pinimg.com/736x/a3/04/16/a30416df4477037a0b37bc7d1c6a7bc3.jpg" alt="">
+</div>
+
+
+
+
+
+
+`
