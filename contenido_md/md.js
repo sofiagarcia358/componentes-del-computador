e290facee3261eaad4c13dd8b6389f78cfac5feb
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-08 19:50:58 -0600

    sofia version

---
+++ contenido_md/md.js
@@ -0,0 +1,39 @@
+let md = document.querySelector(".section_md");
+md.innerHTML =`
+<h1 class="titulo">MD</h1>
+
+<h2 class="Que">¿Que es un archivo MD?</h2>
+
+<div class="info">Los archivos MD son básicamente archivos de texto.
+<br> 
+El texto incluido está escrito en uno de los numerosos distintos dialectos 
+<br>
+del lenguaje de desarrollo Markdown. Se utilizan para escribir documentación
+<br> 
+también en texto sin formato, por lo que pueden convertirse al formato HTML.
+<br> 
+Es más, los archivos MD se utilizan para controla las versiones de código
+<br> 
+fuente, pues facilitan la comparación del código en las revisiones escritas
+<br>
+en texto legible por el ser humano.
+</div>
+
+<div class=info2>
+95% de todos los archivos MD son archivos Markdown Documentation.
+<br>
+El contenido de estos archivos puede incluso ser leído por un humano. 
+<br>
+El tamaño de archivo promedio es 2 KB y la mayoría de los archivos tienen
+<br>
+un tamaño entre 180 bytes y 14 KB. Es un tipo de archivo moderno. 
+<br>
+Ejemplos de nombres de archivo son README.md.
+</div>
+
+<div class="caja">
+<img src="https://www.ionos.es/digitalguide/fileadmin/DigitalGuide/Teaser/md-file-t.jpg" alt="">
+</div>
+
+
+`;
---
+++ contenido_ram/ram.js
@@ -0,0 +1,25 @@
+let ram =document.querySelector(".section_ram");
+ram.innerHTML =`
+<h1 class="ram1">RAM</h1>
+
+<h2 class="ram2">¿Qué significa RAM?</h2>
+
+<div class="text">
+El apelativo de memoria “RAM” sale de su acrónimo en inglés:
+<br>
+Random Access Memory, que en español se traduce a memoria de acceso aleatorio. 
+<br>
+Esta tarjeta es la primordial en los artefactos con software, pues recoge las
+<br>
+referencias de los programas que se están ejecutando en un primer y segundo término.
+</div>
+
+<h2 class="ram3">ENTONCES, ¿CÓMO FUNCIONA UNA MEMORIA RAM?</h2>
+
+<div class="text2">
+La memoria RAM es la encargada de recolectar datos de los programas que el dispositivo esté procesando en el momento que se mantenga encendido. En otras palabras, es la delegada de administrar el empleo de programas, datos y la información que estos arrojen, creando así una base de datos inmediata.</div>
+<div class="cuadrito">
+<img src="https://d22k5h68hofcrd.cloudfront.net/magefan_blog/Que_es_la_memoria_DRAM.jpg" alt="">
+</div>
+
+`;
