0e5580436f4cba73180b52707d6d45cbf73ce2d9
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-14 19:57:54 -0600

    sofias 5

--- footer/footer.js
+++ footer/footer.js
@@ -1,4 +1,33 @@
+let footer_sof = document.querySelector(".footer");
+footer_sof.innerHTML =  `
+<div class="scl">
+<img src="https://media.licdn.com/dms/image/C560BAQFReQup08LukQ/company-logo_200_200/0/1652286228623?e=2147483647&v=beta&t=xzdEhIpadvcEIWVtHBYt0B0bF8ACNmrZ-uhBKH-Ta80" alt="">
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+<ul class="icons">
+  <li><i class="fa-brands fa-youtube"></i> YouTube</li>
+  <li><i class="fa-brands fa-twitter"></i> Twitter</li>
+  <li><i class="fa-brands fa-instagram"></i> Instagram</li>
+</ul>
+
+<ul class="names">
+  <li>Keneth Calderon</li>
+  <li>Sofía García</li>
+</ul>
+<h2 class="d">Desarolladores</h2>
+
+<h2 class="nick">nick names</h2>
+
+<ul class="names2">
+  <li>kenn</li>
+  <li>Sofis</li>
+</ul>
+<h2 class="grado">grado</h2>
+<ul class="names3">
+  <li>cuarto computacion</li>
+</ul>
+`;
 
 
 
