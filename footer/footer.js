dd7f0d0e9d19a3ee3b3c895d49ae92eff99d73c0
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-14 19:32:07 -0600

    Sofia version4

--- footer/footer.js
+++ footer/footer.js
@@ -1,9 +1,4 @@
-let footer_sof = document.querySelector(".footer");
-footer_sof.innerHTML =  `
-<div class="frase">Bill Gates Dijo una vez:"El ordenador nació para resolver problemas que antes no existían". </div>
-<div class="participantes">kennet Calderon <br> Sofía Garcías</div>
 
-`;
 
 
 
